f8b8a1483c0e4319a7eb17c06bdfdcb27b2923e7
Mohit Kumar Bhadani <38796766+Mohit6294@users.noreply.github.com>
2024-08-29 16:27:13 +0530

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDY-lidlA0ZECsJphP-xUe51crc7cJnVxw",
-  authDomain: "mohit-netflix-gpt.firebaseapp.com",
-  projectId: "mohit-netflix-gpt",
-  storageBucket: "mohit-netflix-gpt.appspot.com",
-  messagingSenderId: "339309032085",
-  appId: "1:339309032085:web:c15e04a777053be214219c",
-  measurementId: "G-GMQRR9Y53T"
+  
 };
 
 // Initialize Firebase
